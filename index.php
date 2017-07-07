f290ecd2eaecaba6b1682969a96d8d7dddb48e3d
nmn31 <Kumar.narender.dx@gmail.com>
2017-07-07 15:12:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -12,18 +12,18 @@ use Twilio\Rest\Client;
 $sid = 'AC520af7047a201c5de086b2f915f37042';
 $token = 'f755cfbe3d61fa85b078cac2f5da031e';
 $client = new Client($sid, $token);
-$call = $client->calls->create(
+/*$call = $client->calls->create(
     "+1805-409-0629", "+918894685455",
     array("url" => "http://demo.twilio.com/docs/voice.xml")
 );
 
-echo $call->sid;
-/*$sms = $client->messages->create(
-    "+918894685450",        //+919478633848   9478144262
+echo $call->sid;*/
+$sms = $client->messages->create(
+    "+918894685455",        //+919478633848   9478144262
     array(
         "from" => "+1805-409-0629",
         "body" => "hello  kida? kithe hai aaj kal"
     )
 );
 echo "<pre>";
-print_r($sms);*/
+print_r($sms);
